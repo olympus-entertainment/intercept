d258ca8524d36674fc80a476e90a616e4552fb3f
Matt Coffin <mcoffin13@gmail.com>
2022-05-18 08:31:14 -0600

    vm_context: type change in sdocpos
    This became ref<sourcedocposref> upstream per @dedmen

--- src/client/headers/shared/types.hpp
+++ src/client/headers/shared/types.hpp
@@ -880,7 +880,7 @@ namespace intercept {
             }
 
             const sourcedocpos& get_current_position() {
-                return sdocpos;
+                return *sdocpos;
             }
 
 
@@ -894,7 +894,7 @@ namespace intercept {
 
             sourcedoc sdoc;
 
-            sourcedocpos sdocpos;  //last instruction pos
+            ref<sourcedocposref> sdocpos;  //last instruction pos
 
             r_string name;  //profiler might like this
 
@@ -1477,7 +1477,7 @@ namespace intercept {
                 eval->_errorType = type;
                 eval->_errorMessage = message;
                 if (current_context)
-                    eval->_errorPosition = current_context->sdocpos;
+                    eval->_errorPosition = *current_context->sdocpos;
             }
 
             /**
